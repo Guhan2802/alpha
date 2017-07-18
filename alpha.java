88eec1ba7a75e37aee5bd5fbe12ad447ef2ee20c
Guhan2802 <k.guhan1997@gmail.com>
2017-07-18 15:05:03 +0530

    Create alpha.java

---
+++ alpha.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class alpha
+{
+piblic static void main(String args[])
+{
+Scaaner s=new Scanner(System.in);
+char c=s.next().toLowerCase();
+if(c>='a' && c<='z')
+System.out.print("alphabet");
+else
+System.our.print("not alphabet");
+}
+}
